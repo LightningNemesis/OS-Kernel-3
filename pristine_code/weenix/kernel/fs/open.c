5b9d1f7585edb58b037600b01ff2b68d11442a52
LightningNemesis <ak.kumar220919@gmail.com>
2024-04-21 21:36:39 -0700

    replaced do_open with ridheesh's

--- pristine_code/weenix/kernel/fs/open.c
+++ pristine_code/weenix/kernel/fs/open.c
@@ -89,6 +89,7 @@ int get_empty_fd(proc_t *p)
  *        exists.
  */
 
+/*
 int do_open(const char *filename, int oflags)
 {
         dbg(DBG_PRINT, "(GRADING2A)\n");
@@ -185,4 +186,96 @@ int do_open(const char *filename, int oflags)
         dbg(DBG_PRINT, "(GRADING2A)\n");
 
         return fileDis;
+}
+*/
+
+// Ridheesh
+int do_open(const char *filename, int oflags)
+{
+        // NOT_YET_IMPLEMENTED("VFS: do_open");
+
+        /* No invalid combinations of O_RDONLY, O_WRONLY, and O_RDWR.  Since
+         * O_RDONLY is stupidly defined as 0, the only invalid possible
+         * combination is O_WRONLY|O_RDWR.
+         *                                              ----vfstest_open()
+         * */
+        // check oflags
+        if ((oflags & O_WRONLY) && (oflags & O_RDWR))
+        {
+                return -EINVAL;
+        }
+
+        // 1. Get the next empty file descriptor.
+        int newfd = get_empty_fd(curproc);
+        // if newfd < 0, get_empty_fd falis
+        // case: -EMFILE
+        if (newfd < 0)
+        {
+                return newfd;
+        }
+
+        // 2. Call fget to get a fresh file_t.
+        file_t *newFile = fget(-1);
+
+        // 3. Save the file_t in curproc's file descriptor table.
+        curproc->p_files[newfd] = newFile;
+
+        //  4. Set file_t->f_mode to OR of FMODE_(READ|WRITE|APPEND) based on
+        //  oflags, which can be O_RDONLY, O_WRONLY or O_RDWR, possibly OR'd with
+        //  O_APPEND.
+        newFile->f_mode = 0;
+
+        if (oflags == O_RDONLY)
+        {
+                newFile->f_mode |= FMODE_READ;
+        }
+        if (oflags & O_WRONLY)
+        {
+                newFile->f_mode |= FMODE_WRITE;
+        }
+        if (oflags & O_RDWR)
+        {
+                newFile->f_mode |= (FMODE_READ | FMODE_WRITE);
+        }
+        if (oflags & O_APPEND)
+        {
+                newFile->f_mode |= FMODE_APPEND;
+        }
+
+        // 5. Use open_namev() to get the vnode for the file_t.
+        vnode_t *resVNode = NULL;
+        int code = open_namev(filename, oflags, &resVNode, NULL);
+        // if code < 0, open_namev() fails
+        // case: -ENOENT, -ENOTDIR, and -ENAMETOOLONG
+        if (code < 0)
+        {
+                // fput the newFile
+                fput(newFile);
+                // remove the newfd from curproc
+                curproc->p_files[newfd] = NULL;
+                return code;
+        }
+
+        // check -EISDIR
+        if (S_ISDIR(resVNode->vn_mode) && ((oflags & O_WRONLY) || (oflags & O_RDWR)))
+        {
+                // decrease resVNode vn_refcount
+                vput(resVNode);
+                // fput the newFile
+                fput(newFile);
+                // remove the newfd from curproc
+                curproc->p_files[newfd] = NULL;
+                return -EISDIR;
+        }
+
+        // even if resVNode is a device special file
+        // there is no need to check -ENXIO
+        // because code >= 0 means resVNode exists
+
+        // 6. Fill in the fields of the file_t.
+        newFile->f_pos = 0;
+        newFile->f_vnode = resVNode;
+
+        // 7. Return new fd.
+        return newfd;
 }
